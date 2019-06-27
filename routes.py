911b05d5a76cf9d8763482b7efb3f7d777d44ef2
ishika23 <52277165+ishika23@users.noreply.github.com>
2019-06-27 02:17:19 -0700

    Update routes.py

--- routes.py
+++ routes.py
@@ -2,7 +2,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user={'username':'Ishika'}
+    user={'username':'Ishika Chhabra'}
     return '''
     <html>
     <head>
